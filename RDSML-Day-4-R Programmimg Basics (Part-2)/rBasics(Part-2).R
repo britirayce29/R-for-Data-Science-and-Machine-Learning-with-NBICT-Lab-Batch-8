955e8a774c820bda55f283169aba077132a1ea1d
Briti Ray <britirayce29@gmail.com>
2025-05-07 22:50:30 +0600

    Day-4 Practice

---
+++ RDSML-Day-4-R Programmimg Basics (Part-2)/rBasics(Part-2).R
@@ -0,0 +1,47 @@
+# Data Types in R
+# 1. Numeric - Integer / Floating Point
+# 2. String / Character - " Briti Ray"/ 'Briti Ray'
+# 3. Logical
+
+a = 11
+class(a)
+b = 7.19
+class(b)
+
+name = "Briti Ray"
+print(name)
+class(name)
+
+c = "52"
+print(a+9)
+
+# Logical Data Type - TRUE, FALSE (T, F)
+class(a>b)
+print(a<b)
+x = 19
+y = 22-3
+print(x==y)
+print(x!=y)
+print(y>=x)
+
+
+
+# Logical Operators in R
+# && Logical Operator
+4 == 4 && 5 == 5
+
+# || Logical Operator
+x !=y || x==y
+
+# Simple Mathematical Operators in R
+z = 6+9-8*2/3
+print(z)
+
+w = (4+3-3)*2/3
+print(w)
+
+y=4^3
+print(y)
+
+11%%5
+11%/%5
