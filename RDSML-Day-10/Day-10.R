f84ea91b993201bcf7ff8dc20f735ff18fd836d5
Briti Ray <britirayce29@gmail.com>
2025-05-28 22:55:23 +0600

    day 10

---
+++ RDSML-Day-10/Day-10.R
@@ -0,0 +1,17 @@
+#install.packages("ggplot2")
+library(ggplot2)
+
+dataset = mtcars
+
+# Generating a basic scatter plot
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# changing the point size and shape
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size = 3, shape = 2)
+
+# Adding the regression line
+ggplot(dataset, aes(x = wt, y = mpg)) +
+  geom_point(size = 3, shape = 2) +
+  geom_smooth()
